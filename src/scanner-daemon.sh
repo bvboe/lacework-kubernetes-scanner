7161d6bbc126daa876cd70527f23f94056289f05
Bjorn Boe <bvboe@pobox.com>
2021-08-16 09:12:49 -0400

    Fixed some bugs...

--- src/scanner-daemon.sh
+++ src/scanner-daemon.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 echo `date` Scanning running images in the background
-docker images --format '{{.Repository}}:{{.Tag}}' | xargs -n 1 ./parse-event.sh &
+docker images --format '{{.Repository}}:{{.Tag}}' | grep -v "<none>" | xargs -n 1 ./parse-event.sh &
 
-echo `date` echo Starting daemon
+echo `date` Starting daemon
 docker events --filter 'event=pull' --format '{{.ID}}' | xargs -n 1 ./parse-event.sh
