71eb7a1491f9c57ee040e680ad272ed7090d5beb
Bjorn Boe <bvboe@pobox.com>
2021-08-16 15:33:44 -0400

    Using the Lacework API for deduping scans

--- src/parse-event.sh
+++ src/parse-event.sh
@@ -20,8 +20,16 @@ then
   exit 0
 fi
 
+echo `date` Checking Lacework API cache
+if [ `lacework api get /api/v1/external/vulnerabilities/container/imageId/$SHA --api_key $LW_API_KEY --api_secret $LW_API_SECRET --account $LW_ACCOUNT_NAME | grep Status | grep NotFound | wc -l` -eq 0 ]
+then
+  echo `date` Scan already in Lacework cache, adding to local cache
+  echo "$SHA" >> $SCAN_CACHE_FILE
+  exit 0
+fi
+
 echo `date` Do scan
-lw-scanner evaluate $IMAGE $TAG -t $ACCESS_TOKEN -n $ACCOUNT_NAME
+lw-scanner evaluate $IMAGE $TAG -t $LW_SCANNER_ACCESS_TOKEN -n $LW_ACCOUNT_NAME
 
 echo `date` Registering scan in cache
 echo "$SHA" >> $SCAN_CACHE_FILE
